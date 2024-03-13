fe06919c8d0d198f5556faa35c9c0758bd45ba0e
JelteZ <jelte.zagema@gmail.com>
2024-03-13 14:59:31 +0100

    Newton() functie

---
+++ Model.py
@@ -0,0 +1,10 @@
+
+G = 6.67384*10**-11 # Gravitatieconstante
+MTitanfall = 750    # Massa van onze Titanfall in kg
+MZon = 
+def Newton(MPlaneet, R):
+    Fg = (G*MTitanfall*MPlaneet)/R**2
+    return Fg
+
+
+
