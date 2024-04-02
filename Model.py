9b696528fea6016d9b75156fb0a08e73e0078d07
JelteZ <jelte.zagema@gmail.com>
2024-04-02 22:02:13 +0200

    Misc updates
    Saturnus iets verder van de zon gezet, Titan en de maan daadwerkelijk om de planeten heen laten draaien, kleurtjes en legenda aan de grafiek toevoegen

--- Model.py
+++ Model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from matplotlib.cm import get_cmap
 
 Pi = np.pi
 G = 6.67384*10**-11 # Gravitatieconstante
@@ -40,7 +41,7 @@ Hemellichamen = {
     },
     "Saturnus": {
         "Massa": 5.6834 * 10**26,  # kg
-        "Baanstraal": 1.427 * 10**11,  # m
+        "Baanstraal": 1.427 * 10**12,  # m
         "Omlooptijd": 29.45 * Seconden_in_een_jaar,  # s
         "Starthoek": 257,  # degrees
     },
@@ -69,6 +70,9 @@ Hemellichamen = {
         "Starthoek": 25,  # degrees (om Aarde)
     },
 }
+# Define a colormap and get colors for each planet
+cmap = get_cmap('tab10')
+colors = [cmap(i) for i in range(len(Hemellichamen))]
 
 def Hoekalfa(x_planeet, y_planeet, x_titanfall, y_titanfall):
     alfa = np.arctan((y_planeet-y_titanfall)/(x_planeet-x_titanfall))
@@ -105,9 +109,26 @@ def Planetenposities(t):
         baanstraal = gegevens["Baanstraal"]
         omlooptijd = gegevens["Omlooptijd"]
         starthoek = gegevens["Starthoek"]
-        x, y = cirkelbeweging(baanstraal, omlooptijd, starthoek, t)
+        if planeet == "Titan":
+            # Correct the initial position of Titan relative to Saturn
+            x_s, y_s = cirkelbeweging(Hemellichamen["Saturnus"]["Baanstraal"], Hemellichamen["Saturnus"]["Omlooptijd"],
+                                       Hemellichamen["Saturnus"]["Starthoek"], t)
+            x, y = cirkelbeweging(baanstraal, omlooptijd, starthoek, t)
+            x += x_s
+            y += y_s
+        elif planeet == "Maan":
+            # Correct the initial position of Moon relative to Earth
+            x_e, y_e = cirkelbeweging(Hemellichamen["Aarde"]["Baanstraal"], Hemellichamen["Aarde"]["Omlooptijd"],
+                                       Hemellichamen["Aarde"]["Starthoek"], t)
+            x, y = cirkelbeweging(baanstraal, omlooptijd, starthoek, t)
+            x += x_e
+            y += y_e
+        else:
+            x, y = cirkelbeweging(baanstraal, omlooptijd, starthoek, t)
         planeten_locaties[planeet] = {"x": x, "y": y}
     return planeten_locaties
+
+
 def bewegingTitanfall(Fg_x, Fg_y, a_x, a_y, v_x, v_y,x_titanfall, y_titanfall):
     a_x = a_x + Fg_x/MTitanfall 
     a_y = a_y + Fg_y/MTitanfall
@@ -116,24 +137,35 @@ def bewegingTitanfall(Fg_x, Fg_y, a_x, a_y, v_x, v_y,x_titanfall, y_titanfall):
     x_titanfall = x_titanfall+ v_x*t
     y_titanfall = y_titanfall + v_y*t
 
-fig, ax = plt.subplots()
-ax.set_xlim(-2e12, 2e12)
-ax.set_ylim(-2e12, 2e12)
-scat = ax.scatter([], [], s=50)
+fig, ax = plt.subplots(figsize = (16,16))
+ax.set_xlim(-2e11, 2e11)
+ax.set_ylim(-2e11, 2e11)
+scat = ax.scatter([], [], s=10)
 all_positions = []
-def init():
-    scat.set_offsets(np.zeros((0, 2)))
-    return scat,
 
+def init():
+    for i, (planet, color) in enumerate(zip(Hemellichamen.keys(), colors)):
+        ax.scatter([], [], s=10, color=color, label=planet)
+        all_positions.append([])  # Initialize all_positions list for each planet
+    return scat,
 
 def update(frame):
     t = frame
     planeten_posities = Planetenposities(t)
-    positions = np.array([[planet["x"], planet["y"]] for planet in planeten_posities.values()])
-    all_positions.append(positions)
-    all_positions_to_plot = np.concatenate(all_positions, axis=0)
-    scat.set_offsets(all_positions_to_plot)
-    return scat,
+    for i, (planet, color) in enumerate(zip(Hemellichamen.keys(), colors)):
+        positions = np.array([[planeten_posities[planet]["x"], planeten_posities[planet]["y"]]])
+        all_positions[i].append(positions)
+        all_positions_to_plot = np.concatenate(all_positions[i], axis=0)
+        scatters[i].set_offsets(all_positions_to_plot)
+    return scatters
 
-ani = FuncAnimation(fig, update, frames=np.arange(0, Seconden_in_een_jaar), init_func=init, blit=True)
+# Create scatter plots for each planet
+scatters = [ax.scatter([], [], s=50, color=color) for color in colors]
+
+# Initialize all_positions list to store positions for each planet
+all_positions = []
+
+# Call FuncAnimation with the updated update() function
+ani = FuncAnimation(fig, update, frames=np.arange(0, 365*24*3600), init_func=init, blit=True)
+plt.legend(loc='upper left')
 plt.show()
