43558e35ffcd2116d8bf5ffe6d41cf2ec4b86b60
JelteZ <jelte.zagema@gmail.com>
2024-04-02 16:40:50 +0200

    Planetenposities(t)
    Ik heb gekookt

--- Model.py
+++ Model.py
@@ -7,45 +7,66 @@ MTitanfall = 750    # Massa van onze Titanfall in kg
 
 x_titanfall = 0
 y_titanfall = 0
-# Massa's hemellichamen in kg
-MZon = 1.9884*10**30
-MAarde = 5.972*10**24
-MMars = 6.4171*10**23
-MJupiter = 1.8982*10**27
-MSaturnus = 5.6834*10**26
-MUranus = 86.8*10**24
-MNeptunus = 1.0243*10**26
-MTitan = 1.345*10**23
-MMaan = 0.0735*10**24
-
-#Baanstraal van de planeten (Bij manen gaat het om de baanstraal om de planeet) in meters
-RAarde = 1.496*10**11
-RMars =  2.279*10**11
-RJupiter = 7.785*10**11
-RSaturnus = 1.427*10**11
-RNeptunus = 4.498*10**12
-RTitan = 1.221931*10**9
-RMaan = 384.4*10**6
-
-# Omlooptijden in seconden
 Seconden_in_een_dag = 24*3600
 Seconden_in_een_jaar = Seconden_in_een_dag*365
-TAarde = 365.256 * Seconden_in_een_dag
-TMars =  687*Seconden_in_een_dag
-TJupiter = 11.86*Seconden_in_een_jaar
-TSaturnus = 29.45*Seconden_in_een_jaar
-TNeptunus = 164.8*Seconden_in_een_jaar
-TTitan = 15.94513889*Seconden_in_een_dag
-TMaan = 27.32*Seconden_in_een_dag
 
-# Starthoek baan in graden zoals op de eenheidscirkel
-PhiAarde = 25
-PhiMars = 329
-PhiJupiter = 185
-PhiSaturnus = 257
-PhiNeptunus = 341
-PhiTitan = 0 #bij de manen (Titan en Maan) gaat het om de starthoek van de maan om de planeet
-PhiMaan = 25
+# Bibliotheek met al onze startwaarden/constanten
+Hemellichamen = {
+    "Zon": {
+        "Massa": 1.9884 * 10**30,  # kg
+        "Baanstraal": None,  # Nvt voor de zon
+        "Omlooptijd": None,  # Nvt voor de zon
+        "Starthoek": None,  # Nvt voor de zon
+    },
+    "Aarde": {
+        "Massa": 5.972 * 10**24,  # kg
+        "Baanstraal": 1.496 * 10**11,  # m
+        "Omlooptijd": 365.256 * Seconden_in_een_dag,  # s
+        "Starthoek": 25,  # degrees
+    },
+    "Mars": {
+        "Massa": 6.4171 * 10**23,  # kg
+        "Baanstraal": 2.279 * 10**11,  # m
+        "Omlooptijd": 687 * Seconden_in_een_dag,  # s
+        "Starthoek": 329,  # degrees
+    },
+    "Jupiter": {
+        "Massa": 1.8982 * 10**27,  # kg
+        "Baanstraal": 7.785 * 10**11,  # m
+        "Omlooptijd": 11.86 * Seconden_in_een_jaar,  # s
+        "Starthoek": 185,  # degrees
+    },
+    "Saturnus": {
+        "Massa": 5.6834 * 10**26,  # kg
+        "Baanstraal": 1.427 * 10**11,  # m
+        "Omlooptijd": 29.45 * Seconden_in_een_jaar,  # s
+        "Starthoek": 257,  # degrees
+    },
+    "Uranus": {
+        "Massa": 86.8 * 10**24,  # kg
+        "Baanstraal": 2.871 * 10**12,  # m
+        "Omlooptijd": 84.01 * Seconden_in_een_jaar,  # s
+        "Starthoek": 244,  # degrees
+    },
+    "Neptunus": {
+        "Massa": 1.0243 * 10**26,  # kg
+        "Baanstraal": 4.498 * 10**12,  # m
+        "Omlooptijd": 164.8 * Seconden_in_een_jaar,  # s
+        "Starthoek": 341,  # degrees
+    },
+    "Titan": {
+        "Massa": 1.345 * 10**23,  # kg
+        "Baanstraal": 1.221931 * 10**9,  # m (om Saturnus)
+        "Omlooptijd": 15.94513889 * Seconden_in_een_dag,  # s (om Saturnus)
+        "Starthoek": 0,  # degrees (om Saturnus)
+    },
+    "Maan": {
+        "Massa": 0.0735 * 10**24,  # kg
+        "Baanstraal": 384.4 * 10**6,  # m (om Aarde)
+        "Omlooptijd": 27.32 * Seconden_in_een_dag,  # s (om Aarde)
+        "Starthoek": 25,  # degrees (om Aarde)
+    },
+}
 
 def Hoekalfa(x_planeet, y_planeet, x_titanfall, y_titanfall):
     alfa = np.arctan((y_planeet-y_titanfall)/(x_planeet-x_titanfall))
@@ -54,7 +75,7 @@ def Hoekalfa(x_planeet, y_planeet, x_titanfall, y_titanfall):
 # De functie voor de zwaartekracht van de planeet
 def Newton(MPlaneet, R, alfa):
 
-    Fg_x = np.cos(alfa)*(G*MTitanfall*MPlaneet)/R**2 #hierin is alfa de hoek tussen de lijn Titanfall - planeet en de x-as
+    Fg_x = np.cos(alfa)*(G*MTitanfall*MPlaneet)/R**2 # Hierin is alfa de hoek tussen de lijn Titanfall - planeet en de x-as
     Fg_y = np.sin(alfa)*(G*MTitanfall*MPlaneet)/R**2
 
     return Fg_x, Fg_y
@@ -68,7 +89,16 @@ def cirkelbeweging(R,T,Phi):
     V = Hoeksnelheid(R,T)
     x = R*np.cos(V*t + Phi)
     y = R*np.sin(V*t + Phi)
-    return [x,y]
+    return x,y
+def Planetenposities(t):
+    planeten_locaties = {}
+    for planeet, gegevens in Hemellichamen.items():
+        massa = gegevens["Massa"]
+        baanstraal = gegevens["Baanstraal"]
+        omlooptijd = gegevens["Omlooptijd"]
+        starthoek = gegevens["Starthoek"]
+        x, y = cirkelbeweging(baanstraal, omlooptijd, starthoek, t)
+        planeten_locaties = {"x": x, "y": y}
 def bewegingTitanfall(Fg_x, Fg_y, a_x, a_y, v_x, v_y,x_titanfall, y_titanfall):
     a_x = a_x + Fg_x/MTitanfall 
     a_y = a_y + Fg_y/MTitanfall
